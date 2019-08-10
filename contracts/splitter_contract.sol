e0af33002fdafb64fc6f1017696f4861ef00bc70
marie <malou@Du.home>
2019-08-10 19:01:48 +0200

    .

--- contracts/splitter_contract.sol
+++ contracts/splitter_contract.sol
@@ -4,14 +4,15 @@ contract Splitter {
     address alice = msg.sender; 
     mapping (address => uint) balance;
 
-    event Sent(address from, address to, uint256 amount);
+    event Sent(address from, address bob, address carol, uint256 amount);
 
     constructor() public payable{}
 
     function sendSplit ( uint256 amount ) public payable{
     require(msg.value <= balance[alice]);
-    amount = msg.value/2* 1 ether;
-    
+    amount = msg.value;
+    uint256 Amount = amount/2* 1 ether;
+
        address payable bob = 0x4FcD08ed2F41541A9bA64c9f423E00c3103CcB0F;
        address payable carol = 0x195CB57EB007eF4073D462aa944556F19843D553;
     
@@ -22,8 +23,7 @@ contract Splitter {
         balance[bob] += amount;
         balance[carol] += amount; 
         
-        emit Sent(alice, bob, amount); 
-        emit Sent(alice, carol, amount);
+        emit Sent(alice, bob, carol, amount); 
         
        }
     }
