e767164ec3046ae6a623e86e9529036d386b3f99
marie <malou@Du.home>
2019-08-17 19:27:39 +0200

    second try

--- contracts/splitter_contract.sol
+++ contracts/splitter_contract.sol
@@ -1,29 +1,32 @@
 pragma solidity ^0.5.0;
 
 contract Splitter {
-    address alice = msg.sender; 
-    mapping (address => uint) balance;
-
+    address alice; 
+    mapping (address => uint) public balance;
+    uint256 amount;
     event Sent(address from, address bob, address carol, uint256 amount);
-
-    constructor() public payable{}
-
-    function sendSplit ( uint256 amount ) public payable{
-    require(msg.value <= balance[alice]);
-    amount = msg.value;
-    uint256 Amount = amount/2* 1 ether;
-
-       address payable bob = 0x4FcD08ed2F41541A9bA64c9f423E00c3103CcB0F;
-       address payable carol = 0x195CB57EB007eF4073D462aa944556F19843D553;
     
-        bob.transfer(amount);
-        carol.transfer(amount); 
-        
+    constructor() public payable{
+        alice = msg.sender; 
+    }
+
+    function sendSplit ( address payable bob, address payable carol) public payable returns(bool success){
+     
+    if(msg.value < 0) {
+       require(msg.value <= balance[alice]);
+        amount = msg.value/2;
+    
         balance[alice] -= msg.value; 
         balance[bob] += amount;
         balance[carol] += amount; 
-        
+    
         emit Sent(alice, bob, carol, amount); 
-        
+            }
+        return true;
        }
-    }
+      function withdraw() public {
+        require(balance[msg.sender]>0);
+        msg.sender.transfer(balance[msg.sender]);
+            }
+       
+}
