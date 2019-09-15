487b7a9836f1060883d29d999e0588dfe6516a15
marie <malou@Du.local>
2019-09-15 16:10:03 +0200

    changed the test for Ownable

--- test/splitter.js
+++ test/splitter.js
@@ -2,48 +2,36 @@ const Splitter = artifacts.require('Splitter');
 const assert = require('assert');
 
 let contractInstance;
+ 
 
 contract('Splitter', (accounts) => {
+    const [alice, bob, carol] = accounts;
      beforeEach("create new instance",async () => {
-        return Splitter.new({ from: alice})
-        .then(function(instance){
-           contractInstance = instance;
-        })
-        
-
-        })
-    let alice = accounts[0]; 
-    let bob = accounts[1];  
-    let carol = accounts[2];  
+        contractInstance = await Splitter.new({from: alice});
+    });  
 
     it('bobs balance should be equal to his original balance, minus the gas price, plus the withdrawn amount', async() => {
-        const amount = web3.utils.toWei(10, 'ether').toString();
-        const balanceBefore = await web3.eth.getBalance(bob).toString();
+        const amount = web3.utils.toWei(0.1, 'ether').toFixed();
+        const balanceBefore = await web3.eth.getBalance(bob).toFixed();
         const hash = await contractInstance.withdraw({ from: bob, value: amount});
-        const balanceAfter = await web3.eth.getBalance(bob).toString();
+        const balanceAfter = await web3.eth.getBalance(bob).toFixed();
         const tx = await web3.eth.getTransaction(hash);
         const receipt = await web3.eth.getTransactionReceipt(hash);
-        const gasCost = tx.gasPrice.mul(receipt.gasUsed).toString(); 
+        const gasCost = tx.gasPrice.mul(receipt.gasUsed).toFixed(); 
 
-        assert(balanceAfter == (balanceBefore-gasCost+amount).toString());
+        assert(balanceAfter == (balanceBefore-gasCost+amount).toFixed());
     });
 
     it('should throw because the owner is not the msg.sender', async() =>{
-        owner = accounts[0];
-        web3.utils.toWei(10, 'ether').toString() = msg.value;
-        accounts[4] = msg.sender;
-        return contractInstance.split(bob, carol);
+        const amount = web3.utils.toWei(0.2, 'ether').toFixed();
+        return contractInstance.split({from: accounts[5], to: [bob, carol], value: amount});
     });
 
     it('The contract should be paused when the pause() function is called', async() => {
-        contractInstance.pause();
-        return LogPause;
+        return contractInstance.pause();
     });
 
-    it('The contract should be killed if the kill() function is called'), async() => {
-        contractInstance.kill();
-        return LogKill; 
-    }
-
-
+    it('The contract should be killed if the kill() function is called', async() => {
+        return contractInstance.kill(); 
+    });
 });
