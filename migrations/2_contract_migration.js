e6ddcf24780642d0242d1d07d44e438b3fdd9638
marie <malou@Du.local>
2019-09-17 21:58:02 +0200

    simplified the deployment

--- migrations/2_contract_migration.js
+++ migrations/2_contract_migration.js
@@ -1,16 +1,11 @@
 var Splitter = artifacts.require("./Splitter.sol");
 var SafeMath = artifacts.require("./SafeMath.sol");
-var Pause = artifacts.require("./Pausable.sol");
-var Kill = artifacts.require("./Killable.sol");
-var Owner = artifacts.require("./Ownable.sol");
+var Pausable = artifacts.require("./Pausable.sol");
+var Killable = artifacts.require("./Killable.sol");
+var Ownable = artifacts.require("./Ownable.sol");
 
 module.exports = function(deployer) {
 
-  deployer.deploy(SafeMath);
-  deployer.deploy(Owner);
-  deployer.deploy(Pause);
-  deployer.deploy(Kill);
-  deployer.link(SafeMath, Splitter, Pause, Kill, Owner);
   deployer.deploy(Splitter);
   
 };
