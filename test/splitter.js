afc86d1777e0a1b459197bc9c46ee9eb5257f7b7
marie <malou@Du.home>
2019-10-09 20:29:49 +0200

    Added a test for the split() function

--- test/splitter.js
+++ test/splitter.js
@@ -2,37 +2,66 @@ const Splitter = artifacts.require('Splitter');
 const assert = require('assert');
 const truffleAssert = require('truffle-assertions');
 
+let contractInstance;
+
+
+
 contract('Splitter', (accounts) => {
-    const [alice, bob, carol, notOwner] = accounts;
+    let [alice, bob, carol, notOwner] = accounts;
+    
+   // this.state.web3.eth.defaultAccount = alice;
+
+    
      beforeEach("create new instance", async () => {
         contractInstance = await Splitter.new( { from: alice } );
     });  
 
     it('bobs balance should be equal to his original balance, minus the gas price, plus the withdrawn amount', async() => {
-        const amount = web3.utils.toWei(0.1, 'ether').toBigNumber();
-        const balanceBefore = (await web3.eth.getBalance(bob)).toBigNumber();
-        const hash = await contractInstance.withdraw({ from: bob, value: amount });
-        const balanceAfter = (await web3.eth.getBalance(bob)).toBigNumber();
+        const toWithdraw = web3.utils.toBN(1, 'ether');
+        const balanceBefore = (await web3.eth.getBalance(bob));
+        await contractInstance.split( { from: alice, to: bob, carol }, { value: toWithdraw } );
+        const hash = await contractInstance.withdraw( { from: bob } );
+        const balanceAfter = (await web3.eth.getBalance(bob));
         const tx = await web3.eth.getTransaction(hash);
         const receipt = await web3.eth.getTransactionReceipt(hash);
-        const gasCost = tx.gasPrice.mul(receipt.gasUsed).toBigNumber(); 
+        const gasCost = tx.gasPrice.mul.toBN(receipt.gasUsed); 
 
-        assert(balanceAfter == (balanceBefore-gasCost+amount).toBigNumber());
+        assert(balanceAfter == (balanceBefore-gasCost+toWithdraw));
+    });
+
+    it('The internal balances of Carol and Bob after executing split() should be equal to half of the amount, alices internal balance should be equal to her original balance minus the split amount.', async() => {
+        const amount = web3.utils.toBN(1, 'ether');
+
+        const alicesBalanceBefore = await contractInstance.balances[0];
+        const bobsBalanceBefore = await contractInstance.balances[1];
+        const carolsBalanceBefore = await contractInstance.balances[2];
+       
+        await contractInstance.split( { from : alice } , { to: bob, carol } );
+
+        const alicesBalanceAfter = await contractInstance.balances[0];
+        const bobsBalanceAfter = await contractInstance.balances[1];
+        const carolsBalanceAfter = await contractInstance.balances[2];
+        
+        assert(alicesBalanceAfter == (alicesBalanceBefore- amount));
+        assert(carolsBalanceAfter == (carolsBalanceBefore + (amount/2)));
+        assert(bobsBalanceAfter == (bobsBalanceBefore + (amount/2)));
+
     });
 
     it('should throw because the owner is not the msg.sender', async() =>{
         await truffleAssert.reverts(
             contractInstance.pause({ from: notOwner }),
-            "Only the Owner can pause this contract."
+            "Only the owner can call this function."
         );
         
     });
 
     it('The contract should be paused when the pause() function is called', async() => {
-        return contractInstance.pause( {from : alice} );
+        return contractInstance.pause( { from : alice } );
     });
 
     it('The contract should be killed if the kill() function is called', async() => {
-        return contractInstance.kill(); 
+        return contractInstance.pause( { from : alice } );
+        return contractInstance.kill( { from : alice } ); 
     });
-});                                        
+});
