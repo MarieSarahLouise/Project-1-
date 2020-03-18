7fb5efe42745d3d6465c359a353499297ba41691
marie <malou@Du.home>
2020-03-18 09:44:00 +0100

    Changed some variable names

--- test/splitter.js
+++ test/splitter.js
@@ -1,8 +1,8 @@
-const Splitter = artifacts.require('Splitter');
-const assert = require('assert');
-const truffleAssert = require('truffle-assertions');
+const Splitter = artifacts.require("Splitter");
+const assert = require("assert");
+const truffleAssert = require("truffle-assertions");
 
-contract('Splitter', (accounts) => {
+contract("Splitter", (accounts) => {
    
     const [alice, bob, carol, notOwner] = accounts;
     let contractInstance;
@@ -10,104 +10,105 @@ contract('Splitter', (accounts) => {
         contractInstance = await Splitter.new( { from: alice } );
     });  
 
-    it('Split should emit the expected event', async function(){
+    it("Split should emit the expected event", async function(){
         const amount = 1000000000;
-        const split = await contractInstance.split( bob, carol, { from: alice, value: amount } );
-        const { logs } = split;
+        const splitObj = await contractInstance.split(bob, carol, { from: alice, value: amount });
+        const { logs } = splitObj;
         assert.strictEqual(logs.length, 1);
-        const events = split.logs[0];
-        assert.strictEqual(events.event, 'LogAmountSent');
-        assert.strictEqual(events.args.from, alice);
-        assert.strictEqual(events.args.bob, bob);
-        assert.strictEqual(events.args.carol, carol);
-        assert.strictEqual(events.args.amount.toString(), amount.toString());
+        const splitEvent = splitObj.logs[0];
+        assert.strictEqual(splitEvent.event, "LogAmountSent");
+        assert.strictEqual(splitEvent.args.from, alice);
+        assert.strictEqual(splitEvent.args.bob, bob);
+        assert.strictEqual(splitEvent.args.carol, carol);
+        assert.strictEqual(splitEvent.args.amount.toString(), amount.toString());
     });
 
-    it('Carol and Bob should have the expected internal balances after split (uneven amounts).', async function() {
-        await contractInstance.split(bob, carol, { from : alice , value: 7 } );
-        const bobsBalanceAfter = await contractInstance.balances(bob);
-        const carolsBalanceAfter = await contractInstance.balances(carol);
-        const alicesBalanceAfter  = await contractInstance.balances(alice);
+    it("Carol and Bob should have the expected internal balances after split (uneven amounts).", async function() {
+        await contractInstance.split(bob, carol, { from : alice , value: 7 });
+        const bobsBalanceAfter = await contractInstance.getBalance(bob);
+        const carolsBalanceAfter = await contractInstance.getBalance(carol);
+        const alicesBalanceAfter  = await contractInstance.getBalance(alice);
 
-        assert.strictEqual(alicesBalanceAfter.toString(), "1", 'alices balance is not right.')
-        assert.strictEqual(carolsBalanceAfter.toString(),  "3", 'carols balance is not right.');
-        assert.strictEqual(bobsBalanceAfter.toString(),  "3",'bobs balance is not right.');
+        assert.strictEqual(alicesBalanceAfter.toString(), "1", "alices balance is not right.");
+        assert.strictEqual(carolsBalanceAfter.toString(), "3", "carols balance is not right.");
+        assert.strictEqual(bobsBalanceAfter.toString(), "3", "bobs balance is not right.");
     });
 
-    it('Carol and Bob should have the expected internal balances after split (even amounts).', async function() {
+    it("Carol and Bob should have the expected internal balances after split (even amounts).", async function() {
         const amount = 1000000000;
-        await contractInstance.split(bob, carol, { from : alice , value: amount } );
-        const bobsBalanceAfter = await (contractInstance.balances(bob));
-        const carolsBalanceAfter = await (contractInstance.balances(carol));
+        await contractInstance.split(bob, carol, { from : alice , value: amount });
+        const bobsBalanceAfter = await (contractInstance.getBalance(bob));
+        const carolsBalanceAfter = await (contractInstance.getBalance(carol));
 
-        assert.strictEqual(carolsBalanceAfter.toString(),  "500000000", 'carols balance is not right.');
-        assert.strictEqual(bobsBalanceAfter.toString(),  "500000000",'bobs balance is not right.');
+        assert.strictEqual(carolsBalanceAfter.toString(),  "500000000", "carols balance is not right.");
+        assert.strictEqual(bobsBalanceAfter.toString(),  "500000000", "bobs balance is not right.");
     });
        
-    it('Withdraw should emit the expected event.', async function(){
-        const amount = 1000000000;
-        await contractInstance.split( bob, carol, { from: alice, value: amount } );
-        let withdraw = await contractInstance.withdraw({from: bob});
+    it("Withdraw should emit the expected event.", async function(){
+        const amountToSplit= 1000000000;
+        await contractInstance.split( bob, carol, { from: alice, value: amountToSplit });
+        const withdraw = await contractInstance.withdraw({ from: bob });
         const log = withdraw.logs[0];
         assert.strictEqual(withdraw.logs.length, 1);
-        assert.strictEqual(log.event, 'LogAmountWithdrawn', 'The right event was not emitted.');
-        assert.strictEqual(log.args.from, bob, 'Bobs adress is not right.');
-        assert.strictEqual(log.args.amount.toString(), "500000000", 'The right amount was not emitted.');
+        assert.strictEqual(log.event, "LogAmountWithdrawn", "The right event was not emitted.");
+        assert.strictEqual(log.args.from, bob, "Bobs adress is not right.");
+        assert.strictEqual(log.args.amount.toString(), "500000000", "The right amount was not emitted.");
     });
 
-    it('Bobs balance should not be reset after each split and Carol should be able to send ether.', async function() {
-        const amount = 1000000000;
-        await contractInstance.split(alice, bob, { from : carol , value: amount });
-        await contractInstance.split(carol, bob, { from: alice, value : amount});
-        const bobsBalanceAfter = await (contractInstance.balances(bob));
+    it("Bobs balance should not be reset after each split and Carol should be able to send ether.", async function() {
+        const amount1 = 1000000000;
+        const amount2 = 500000000;
+        await contractInstance.split(alice, bob, { from : carol , value: amount1 });
+        await contractInstance.split(carol, bob, { from: alice, value : amount2 });
+        const bobsBalanceAfter = await (contractInstance.getBalance(bob));
 
-        assert.strictEqual(bobsBalanceAfter.toString(),  "1000000000",'bobs balance is not right.');
+        assert.strictEqual(bobsBalanceAfter.toString(),  "750000000", "bobs balance is not right.");
     });
 
-    it('Bobs Balance after withdraw should be right.', async function(){
+    it("Bobs Balance after withdraw should be right.", async function(){
         const amount = 1000000000;
         const balanceBefore = await web3.eth.getBalance(bob);
-        await contractInstance.split( bob, carol, { from: alice, value: amount } );
-        const txObj = await contractInstance.withdraw( { from: bob } );
+        await contractInstance.split( bob, carol, { from: alice, value: amount });
+        const txObj = await contractInstance.withdraw({ from: bob });
         const tx = await web3.eth.getTransaction(txObj.tx);
         const receipt = txObj.receipt;
         const balanceAfter = await web3.eth.getBalance(bob);
         const gasCost = (tx.gasPrice)*(receipt.gasUsed);
-        const expectedBalanceAfter = balanceBefore -  gasCost + 500000000;
+        const expectedBalanceAfter = balanceBefore - gasCost + 500000000;
 
-        assert.strictEqual(balanceAfter.toString(), expectedBalanceAfter.toString(), 'bobs balance is not right.');
+        assert.strictEqual(balanceAfter.toString(), expectedBalanceAfter.toString(), "bobs balance is not right.");
     });
 
-    it('Carols and Bobs internal balances after withdrawing should be 0.', async function(){
+    it("Carols and Bobs internal balances after withdrawing should be 0.", async function(){
         const amount = 1000000000;
-        await contractInstance.split(bob, carol, { from: alice, value: amount});
+        await contractInstance.split(bob, carol, { from: alice, value: amount });
         await contractInstance.withdraw({ from: carol });
-        const carolsBalanceAfter = await (contractInstance.balances(carol));
+        const carolsBalanceAfter = await (contractInstance.getBalance(carol));
         
         await contractInstance.withdraw({ from: bob });
-        const bobsBalanceAfter = await (contractInstance.balances(bob));
+        const bobsBalanceAfter = await (contractInstance.getBalance(bob));
          
-        assert.strictEqual(bobsBalanceAfter.toString(), "0", 'bobs balance is not right.');
-        assert.strictEqual(carolsBalanceAfter.toString(), "0", 'carols balance is not right.');
+        assert.strictEqual(bobsBalanceAfter.toString(), "0", "bobs balance is not right.");
+        assert.strictEqual(carolsBalanceAfter.toString(), "0", "carols balance is not right.");
     });
 
-    it('Should not allow to pause, because the owner is not the msg.sender.', async function() {
+    it("Should not allow to pause, because the owner is not the msg.sender.", async function() {
         await truffleAssert.reverts(
-            contractInstance.pause( { from: notOwner } ),
+            contractInstance.pause({ from: notOwner }),
             "Only the owner can call this function."
         );
     });
 
-    it('the Splitter should start un-paused.', async function() {
+    it("the Splitter should start un-paused.", async function() {
         assert.strictEqual(await contractInstance.isPaused(), false);
     });
 
-    it('The contract should be paused when the pause() function is called.', async function() {
+    it("The contract should be paused when the pause() function is called.", async function() {
         await contractInstance.pause({ from : alice });
         assert.strictEqual(await contractInstance.isPaused(), true);
     });
 
-    it('The contract should be killed if the kill() function is called.', async function() {
+    it("The contract should be killed if the kill() function is called.", async function() {
         await contractInstance.pause({ from : alice });
         await contractInstance.kill({ from : alice });
         assert.strictEqual(await contractInstance.isKilled(), true);
