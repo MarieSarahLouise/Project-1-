b6aa75515bcb9dfddfe677b6b912e205b19dc372
marie <malou@Du.home>
2020-04-13 16:49:40 +0200

    added some tests for the pause function

---
+++ test/pausable.js
@@ -0,0 +1,54 @@
+const Splitter = artifacts.require("Splitter");
+const assert = require("assert");
+const truffleAssert = require("truffle-assertions");
+const { toWei, toBN } = web3.utils;
+
+contract("Splitter", (accounts) => {
+   
+    const [alice, bob, carol, notOwner] = accounts;
+    let contractInstance;
+     beforeEach("create new instance", async function() {
+        contractInstance = await Splitter.new( { from: alice } );
+    });
+
+    it("The contract should be paused when the pause() function is called.", async function() {
+        await contractInstance.pause({ from: alice });
+        assert.strictEqual(await contractInstance.isPaused(), true);
+    });
+
+    it("The contract should only be paused by the Owner.", async function() {
+        await truffleAssert.reverts(
+            contractInstance.pause({ from: notOwner }), 
+            "Only the owner can call this function.")
+    });
+
+    it("The contract should not be paused twice.", async function(){
+        await contractInstance.pause({ from: alice });
+        await truffleAssert.reverts(
+            contractInstance.pause({ from: alice }), 
+            "The contract is paused.")
+    });
+
+    it("Alice shouldn't be able to send ether when the contract is paused.", async function() {
+        const amount = toWei(toBN("1"), "Gwei");
+        await contractInstance.pause({ from: alice });
+        await truffleAssert.reverts(
+            contractInstance.split(bob, carol, { from: alice, value: amount }),
+            "The contract is paused."
+        );
+    });
+
+    it("The contract should start again, if the resume function is called.", async function() {
+        await contractInstance.pause({ from: alice });
+        await contractInstance.resume({ from: alice });
+        assert.strictEqual(await contractInstance.isPaused(), false);
+    });
+
+    it("Only the ownwe should resume th contract.", async function() {
+        await contractInstance.pause({ from: alice });
+        await truffleAssert.reverts(
+            contractInstance.resume({ from: notOwner }), 
+            "Only the owner can call this function.")
+    })
+
+});
